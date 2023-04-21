d4ea838c8ef1b82f5cef33e42669ef591407cfe1
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2023-04-21 20:09:12 +0100

    Add cetuximab data

--- R/datasets.R
+++ R/datasets.R
@@ -36,3 +36,69 @@
 ##'
 ##' @keywords datasets
 "curedata"
+
+
+##' Datasets for evaluation of cetuximab in head and neck cancer
+##'
+##' Datasets for evaluation of cetuximab in head and neck cancer, as previously
+##' analysed by Guyot et al. (2017) to demonstrate models for survival extrapolation
+##' with Bayesian evidence synthesis.
+##'
+##' @aliases cetux cetux_seer cetux_bh
+##'
+##' 
+##' @format \code{cetux} contains synthetic individual-level survival
+##'   data, generated (using the method of Guyot et al 2012) to be
+##'   consistent with the Kaplan-Meier estimates of survival published
+##'   by Bonner et al. (2006).
+##' Columns \code{months}, \code{years}, give survival in months or
+##' years since the date of diagnosis of head and neck cancer.
+##' \code{d} is a numeric vector where 0 indicates censoring, and 1
+##' indicates death at this time.  \code{treat} is a factor indicating
+##' the treatment group (Cetuximab or Control); both groups also
+##' received radiotherapy.
+##'
+##' \code{cetux_seer} Estimates of conditional survival from registry data, 
+##' matched to the Bonner trial population by age, gender, cancer site, and date of diagnosis.
+##' From the "Surveillance Epidemiology and End Results" (SEER) database.
+##' Each line gives counts of \code{r} survivors up to \code{stop} years, 
+##' given \code{n} people alive at \code{start}.  \code{haz} is the
+##' corresponding constand hazard estimate over this period. 
+##'
+##' \code{cetux_bh} Mortality rates for the population of the
+##' USA, matched by age and sex to the patients from the Bonner trial.
+##' 80% are male, and the median age is 57 (range 34 to 83).  Hence
+##' the \eqn{i}th row is a weighted average of the male and 
+##' and female mortality rates for age \eqn{57 + i - 1}.
+##'
+##' See Guyot et al. (2017) for more details of each of these.
+##'
+##' @references 
+##'
+##' Guyot, P., Ades, A.E., Beasley, M., Lueza, B., Pignon, J.P. and
+##' Welton, N.J., (2017). Extrapolation of survival curves from cancer
+##' trials using external information. Medical Decision Making, 37(4),
+##' pp.353-366.
+##'
+##' Bonner, J.A., Harari, P.M., Giralt, J., Azarnia, N., Shin, D.M.,
+##' Cohen, R.B., Jones, C.U., Sur, R., Raben, D., Jassem, J. and Ove,
+##' R., (2006) Radiotherapy plus cetuximab for squamous-cell carcinoma
+##' of the head and neck. New England Journal of Medicine, 354(6),
+##' pp.567-578.
+##'
+##' Guyot, P., Ades, A.E., Ouwens, M.J. and Welton, N.J.,
+##' (2012) Enhanced secondary analysis of survival data: reconstructing
+##' the data from published Kaplan-Meier survival curves. BMC Medical
+##' Research Methodology, 12, pp.1-13.
+##'
+##' @name cetux
+NULL
+
+##' @rdname cetux
+"cetux"
+
+##' @rdname cetux
+"cetux_seer"
+
+##' @rdname cetux
+"cetux_bh"
---
+++ data-raw/cetux.r
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+cetux <- read.table("data-raw/ipd_recon.dat",header = TRUE) %>%
+  mutate(treat = fct_recode(factor(treat), "Control"="0", "Cetuximab"="1"),
+         years = t / 12) %>%
+  rename(months = t) %>%
+  select(months, years, d, treat)
+
+cetux_seer <- read.table("../paper/guyot_data/seer.dat",skip=10,
+                         col.names=c("t","num","denom"),nrows = 21) %>%
+  mutate(start = t-1) %>%
+  select(start,stop=t,r=num,n=denom) %>%
+  mutate(treat = factor("Control"))
+
+agemed <- 57
+agesd <- (83 - 34)/6 # interpret range as 6 SDs. not used
+p_male <- 0.8
+# Use mortality rate for median age at randomisation + years of follow up
+# averaged over gender balance at rand
+cetux_bh <- read.table("../paper/guyot_data/mort_usa.txt",header=TRUE,skip=2) %>%
+  filter(Year==1999) %>%
+  mutate(Age = as.numeric(ifelse(Age=="110+", "110", Age)),
+         Agenext = ifelse(Age==110, Inf, Age+1),
+         Trialyear = Age - agemed,
+         hazard = p_male*Male + (1 - p_male)*Female) %>%
+  filter(Trialyear >= 0) %>%
+  select(time=Trialyear, hazard)
+
+use_data(cetux, overwrite=TRUE)
+use_data(cetux_seer, overwrite=TRUE)
+use_data(cetux_bh, overwrite=TRUE)
