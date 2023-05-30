47fc434d37f5ec2a764efe003fe6c09d046ef937
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2023-05-30 14:06:50 +0100

    External data test cases

--- tests/testthat/test_external.R
+++ tests/testthat/test_external.R
@@ -1,1 +1,28 @@
 ## TODO tests with external data
+
+test_that("External data gives precise extrapolation",{
+  extdat <- data.frame(start = c(5, 10, 15, 20),
+                       stop =  c(10, 15, 20, 25),
+                       n = c(100, 100, 100, 100),
+                       r = c(50, 40, 30, 20))
+  nde_mod <- survextrap(Surv(years, status) ~ 1, data=colons,
+                        chains=1, external = extdat,
+                        add_knots=c(4, 10, 25), fit_method="opt")
+  expect_true(hazard(nde_mod, t=20)$upper < 0.5)
+  expect_true(all(c(4,10,25) %in% nde_mod$mspline$knots))
+})
+
+test_that("External data with covariates",{
+  extdat$sex = 1
+  nde_mod1 <- survextrap(Surv(years, status) ~ sex, data=colons,
+                         chains=1, external = extdat,
+                         add_knots=c(4, 10, 25), fit_method="opt")
+  haz1 <- hazard(nde_mod1, newdata=list(sex=c(0,1)), t=20)
+  extdat$sex = 0
+  nde_mod0 <- survextrap(Surv(years, status) ~ sex, data=colons,
+                         chains=1, external = extdat,
+                         add_knots=c(4, 10, 25), fit_method="opt")
+  haz0 <- hazard(nde_mod0, newdata=list(sex=c(0,1)), t=20)
+  ## more precise estimate given external data with the desired covariate value
+  expect_true(haz1$upper[haz1$sex==1] < haz0$upper[haz0$sex==1])
+})
