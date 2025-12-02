3b48cd7440047ba425dcd9ec68a1759b60f6762a
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2025-12-02 11:22:11 +0000

    Fix for overflow in LOO

--- R/loo.R
+++ R/loo.R
@@ -10,14 +10,20 @@
 #'
 #' @return Result of call to \code{loo::loo}
 #'
-#' @noRd 
+#' @noRd
 loo_survextrap <- function(x, standata, loglik_fn){
   ll <- loglik_fn(x, standata)
   chains <- x$stanfit@sim$chains
   iter <- NROW(ll)
   chain_id <- rep(1:chains, each=iter/chains)
-  r_eff <- loo::relative_eff(exp(ll), chain_id=chain_id, cores=1)
-  res <- loo::loo(ll, r_eff = r_eff)
+  if (any(is.nan(exp(ll)))){
+    warning("NaN produced for likelihood when computing LOO statistic")
+    res <- NA
+  }
+  else {
+    r_eff <- loo::relative_eff(exp(ll), chain_id=chain_id, cores=1)
+    res <- loo::loo(ll, r_eff = r_eff)
+  }
   res
 }
 
@@ -29,7 +35,7 @@ loo_survextrap <- function(x, standata, loglik_fn){
 #'   (ordered with event times before censoring times) and one row per
 #'   MCMC sample.
 #'
-#' @noRd 
+#' @noRd
 loglik_ipd <- function(x, standata){
   stanmat <- get_draws(x)
   if (standata$nevent > 0) {
@@ -54,7 +60,7 @@ loglik_ipd <- function(x, standata){
 }
 
 #' Posterior log-likelihoods for the external data
-#' 
+#'
 #' @details
 #'
 #' Aggregated likelihood is \eqn{r \sim Bin(n, pstop/pstart)}.
@@ -70,7 +76,7 @@ loglik_ipd <- function(x, standata){
 #' @return Matrix with one column per individual in the disaggregated
 #'   external data, and one row per MCMC sample.
 #'
-#' @noRd 
+#' @noRd
 loglik_external <- function(x, standata){
   stanmat <- get_draws(x)
   coefs <- get_coefs_bycovs(x, stanmat, X=standata$xnph_ext)
@@ -86,9 +92,9 @@ loglik_external <- function(x, standata){
   lp_start <- log_surv(alpha,  standata$ibasis_ext_start, coefs,
                        standata$cure, pcure, modelid=1) +
     rep(log(standata$backsurv_ext_start), each=niter)
-  lp_stop <- lp_stop[,inds] # niter rows x ni cols
-  lp_start <- lp_start[,inds]
-  y <- matrix(y, nrow=1)[rep(1,niter),] # niter x ni
+  lp_stop <- lp_stop[,inds,drop=FALSE] # niter rows x ni cols
+  lp_start <- lp_start[,inds,drop=FALSE]
+  y <- matrix(y, nrow=1)[rep(1,niter),,drop=FALSE] # niter x ni
   y*(lp_stop - lp_start) + (1 - y)*log(1 - exp(lp_stop - lp_start))
 }
 
